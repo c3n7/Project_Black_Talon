da16e92635057426a05672d6b5ede0d0f873618b
Timothy Karani <kartimothy@gmail.com>
2020-03-11 14:24:23 +0300

    Created an alert showing function

--- lib/pages/contractors.dart
+++ lib/pages/contractors.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:Project_Black_Talon/navigationdrawer.dart';
 
 class ContractorsScreen extends StatelessWidget {
+  Future<void> _showAlert(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Rewind and remember'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('You will never be satisfied.'),
+                Text('You\'re like me. I\'m never satisfied.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Regret'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
