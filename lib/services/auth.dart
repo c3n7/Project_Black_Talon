a71234633293efff9315c5e16d68b9e5a25a5d91
Pakamee <ruthkeggy@gmail.com>
2020-03-18 14:33:46 +0300

    Writing data to the database

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 Future signInAuth(String _email, String _password, BuildContext context) async {
   FirebaseAuth _auth = FirebaseAuth.instance;
@@ -27,3 +29,40 @@ Future signUpAuth(String _email, String _password) async {
     return null;
   }
 }
+
+//function used in reports.
+Future readData() async {
+  return await Firestore.instance.collection('').getDocuments();
+}
+
+//function used to perform search queries
+Future getData() async {
+  return await Firestore.instance
+      .collection('')
+      .where('Uid', isEqualTo: '')
+      .getDocuments();
+}
+
+class CrudMethods {
+  bool isLoggedIn() {
+    if (FirebaseAuth.instance.currentUser() != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> addData(db) async {
+    if (isLoggedIn()) {
+      return await Firestore.instance
+          .collection('key_book')
+          .add(db)
+          .catchError((e) {
+        print(e);
+      });
+    } else {
+      print('You are not logged in');
+    }
+  }
+}
+
