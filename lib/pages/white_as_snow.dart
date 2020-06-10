269e5a688b2d92b1cd3493e21f0f109b1eed9bd8
Timothy Karani <kartimothy@gmail.com>
2020-06-10 07:01:58 +0300

    Bumped up firebase dependencies

--- lib/pages/white_as_snow.dart
+++ lib/pages/white_as_snow.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
 import 'package:Project_Black_Talon/navigationdrawer.dart';
 
-class WhiteAsSnowScreen extends StatelessWidget {
+class WhiteAsSnowScreen extends StatefulWidget {
+  @override
+  _WhiteAsSnowState createState() => new _WhiteAsSnowState();
+}
+
+class _WhiteAsSnowState extends State<WhiteAsSnowScreen> {
+  String barcode = "";
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +23,22 @@ class WhiteAsSnowScreen extends StatelessWidget {
       ),
       drawer: NavigationDrawer(),
       body: Center(
-        child: Text('White as Snow'),
+        // child: Text('White as Snow'),
+        child: RaisedButton(
+          color: Theme.of(context).primaryColor,
+          textColor: Colors.white,
+          onPressed: scan,
+          child: this.barcode != "" ? Text(this.barcode) : Text('Scan Text'),
+        ),
       ),
     );
+  }
+
+  Future scan() async {
+    ScanResult barcode = await BarcodeScanner.scan();
+    setState(() {
+      this.barcode = barcode.rawContent.toString();
+      print(this.barcode);
+    });
   }
 }
